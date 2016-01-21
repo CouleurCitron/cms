c5828610794af1ac08a24457eb3011500c7641a5
Raphael <contact@raphael-goncalves.fr>
2016-01-21 14:21:06 +0100

    MAJ securite

--- utils/info.php
+++ utils/info.php
@@ -1,5 +1,11 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
+
+if($_SESSION['login'] != 'ccitron')
+{
+    exit('Not allowed.');
+}
+
 pre_dump(iconv_get_encoding("all"));
 pre_dump(get_include_path());
 phpinfo();
