4e98e47ca3d8454de89ea3e5cc1f74d5ddb8ac6f
Pierre Priot <pierre@couleur-citron.com>
2018-05-31 10:27:02 +0200

    passage en recaptcha v2

--- formulaire/captcha.php
+++ formulaire/captcha.php
@@ -1,18 +1,33 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
 	
-	require_once('cms-inc/lib/recaptcha/recaptchalib.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/recaptcha/recaptchalib.php');
 	
-	$resp = recaptcha_check_answer ($_POST["privatekey"],
-								$_SERVER["REMOTE_ADDR"],
-								$_POST["recaptcha_challenge_field"],
-								$_POST["recaptcha_response_field"]);
- 
-	if (!$resp->is_valid) {
-		echo  0;
-	}
-	else {
-		echo 1;
-	}
+if (preg_match('/^192\.168\./si', $_SERVER["REMOTE_ADDR"])){
+	$remoteip = '37.1.253.217';
+}
+else{
+	$remoteip = $_SERVER["REMOTE_ADDR"];
+}
 
-?>
+$resp = recaptcha_check_answer ($_POST["privatekey"],
+							$remoteip,
+								$_POST["recaptcha_challenge_field"],
+							$_POST["recaptcha_response_field"],
+							array(),
+							$_POST["recaptcha_version"]);
+ 
+if (!$resp->is_valid) {
+		echo  0;
+
+	/* error_log('recaptcha '.$resp->error);
+	echo "--------------<br />";
+	echo $resp->error;
+
+	echo "<pre> _POST: =========\n";
+	print_r($_POST);
+	echo "\n=========\n</pre>"; */
+}
+else {
+		echo 1;
+}
