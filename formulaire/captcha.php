f15dfd0d457ffdc235dac0507613c7d44bc1124a
Pierre Priot <pierre@couleur-citron.com>
2018-07-26 18:10:19 +0200

    passage en recaptcha v2

--- formulaire/captcha.php
+++ formulaire/captcha.php
@@ -1,8 +1,14 @@
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
-	
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/recaptcha/recaptchalib.php');
 
+if(defined('DEV_RECAPTCHA_SECRETKEY')){
+	$privatekey=DEV_RECAPTCHA_SECRETKEY;
+}
+else{
+	$privatekey=$_POST["privatekey"];
+}
+	
 if (preg_match('/^192\.168\./si', $_SERVER["REMOTE_ADDR"])){
 	$remoteip = '37.1.253.217';
 }
@@ -10,7 +16,7 @@ else{
 	$remoteip = $_SERVER["REMOTE_ADDR"];
 }
 
-$resp = recaptcha_check_answer ($_POST["privatekey"],
+$resp = recaptcha_check_answer (	$privatekey,
 							$remoteip,
 							$_POST["recaptcha_challenge_field"],
 							$_POST["recaptcha_response_field"],
@@ -19,9 +25,8 @@ $resp = recaptcha_check_answer ($_POST["privatekey"],
 
 if (!$resp->is_valid) {
 	echo  0;
-
-	/* error_log('recaptcha '.$resp->error);
-	echo "--------------<br />";
+	//error_log('recaptcha '.$resp->error);
+	/*echo "--------------<br />";
 	echo $resp->error;
 
 	echo "<pre> _POST: =========\n";
