b8318f1d51cbc587dd229c0bfd03ada1e73d5d43
Pierre Priot <pierre@couleur-citron.com>
2018-10-04 15:12:59 +0200

    use defined key rather than expect posted var

--- formulaire/captcha.php
+++ formulaire/captcha.php
@@ -3,6 +3,13 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/include/autoprepend.php');
 	
 require_once($_SERVER['DOCUMENT_ROOT'].'/include/cms-inc/lib/recaptcha/recaptchalib.php');
 	
+if(defined('DEV_RECAPTCHA_SECRETKEY')){
+	$privatekey=DEV_RECAPTCHA_SECRETKEY;
+}
+else{
+	$privatekey=$_POST["privatekey"];
+}
+
 if (preg_match('/^192\.168\./si', $_SERVER["REMOTE_ADDR"])){
 	$remoteip = '37.1.253.217';
 }
@@ -10,9 +17,9 @@ else{
 	$remoteip = $_SERVER["REMOTE_ADDR"];
 }
 
-$resp = recaptcha_check_answer ($_POST["privatekey"],
+$resp = recaptcha_check_answer ($privatekey,
 							$remoteip,
-								$_POST["recaptcha_challenge_field"],
+							$_POST["recaptcha_challenge_field"],
 							$_POST["recaptcha_response_field"],
 							array(),
 							$_POST["recaptcha_version"]);
