e5eb5be45a73cb6e60d8c9019dc7aa83be74f4ea
Pierre Priot <pierre@couleur-citron.com>
2019-04-30 15:44:01 +0200

    dont check valid recaptcha more than once

--- formulaire/captcha.php
+++ formulaire/captcha.php
@@ -9,7 +9,7 @@ elseif(is_post("privatekey")){
 	$privatekey=$_POST["privatekey"];
 }
 else{
- error_log('recaptcha cant work wihout privatekey - DEV_RECAPTCHA_SECRETKEY');
+  error_log('recaptcha cant work wihout privatekey - DEV_RECAPTCHA_SECRETKEY');
 }
 
 if (preg_match('/^192\.168\./si', $_SERVER["REMOTE_ADDR"])){
@@ -19,24 +19,35 @@ else{
 	$remoteip = $_SERVER["REMOTE_ADDR"];
 }
 
-$resp = recaptcha_check_answer ($privatekey,
+if (!isset($_SESSION['recaptchareqs'])){
+  $_SESSION['recaptchareqs']=array();
+}
+$reqNonce = md5($remoteip.$_POST["recaptcha_response_field"].$_POST["recaptcha_version"]);
+
+// dont check valid recaptcha more than once
+if (!isset($_SESSION['recaptchareqs'][$reqNonce])){
+    $resp = recaptcha_check_answer ($privatekey,
 							$remoteip,
 							$_POST["recaptcha_challenge_field"],
 							$_POST["recaptcha_response_field"],
 							array(),
 							$_POST["recaptcha_version"]);
+  
+    if (!$resp->is_valid) {
+      echo  0;
+      error_log(implode("\n", $resp->error));
+      /*echo "--------------<br />";
+      echo $resp->error;
 
-if (!$resp->is_valid) {
-	echo  0;
-  error_log($resp->error);
-	/*echo "--------------<br />";
-	echo $resp->error;
-
-	echo "<pre> _POST: =========\n";
-	print_r($_POST);
-	echo "\n=========\n</pre>"; */
+      echo "<pre> _POST: =========\n";
+      print_r($_POST);
+      echo "\n=========\n</pre>"; */
+    }
+    else {
+      $_SESSION['recaptchareqs'][$reqNonce]=1;
+      echo 1;
+    }
 }
-else {
-	echo 1;
+else{
+  echo 1;  
 }
-
